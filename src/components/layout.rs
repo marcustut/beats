1124354dd967283643a296f56c87e5d9f6de1cff
Marcus Lee <marcustutorial@hotmail.com>
2022-06-23 14:29:24 +0800

    saved before deleting

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -38,6 +38,22 @@ pub fn layout(props: &LayoutProps) -> Html {
             link: String::from("https://www.youtube.com/watch?v=eAs4Z_9I5LA"),
             name: String::from("yihuik - estrangement"),
         },
+        YouTubeVideo {
+            link: String::from("https://www.youtube.com/watch?v=a7fzkqLozwA"),
+            name: String::from("lauv - i like me better"),
+        },
+        YouTubeVideo {
+            link: String::from("https://www.youtube.com/watch?v=6a3Pwb_c6fI&list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA&start_radio=1&rv=a7fzkqLozwA"),
+            name: String::from("priscilla abby - 夜空中最亮的星"),
+        },
+        YouTubeVideo {
+            link: String::from("https://www.youtube.com/watch?v=3MeCh9OapjE"),
+            name: String::from("My菜 - 菲道尔"),
+        },
+        YouTubeVideo {
+            link: String::from("https://www.youtube.com/watch?v=dos9LST6kbU"),
+            name: String::from("菲道尔 Firdhaus【月亮失約了Absent Moon】"),
+        },
     ];
 
     {
